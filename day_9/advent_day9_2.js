f08101e42b5653f138ecffc8acb3aedc9e97234b
kevinkzx <80101792+kevinkzx@users.noreply.github.com>
2024-12-11 13:46:59 +0800

    advent day 9 p2
    day 9 p2

--- day_9/advent_day9_2.js
+++ day_9/advent_day9_2.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-let input = fs.readFileSync("advent_day9_custom.txt").toString();
-console.log("input: ", input);
+let input = fs.readFileSync("advent_day9.txt").toString();
+// console.log("input: ", input);
 
 let temp = [];
 for (let i = 0; i < input.length; i += 2) {
@@ -16,7 +16,7 @@ for (let i = 0; i < input.length; i += 2) {
     }
   }
 }
-console.log("temp: ", temp);
+// console.log("temp: ", temp);
 let last = 0;
 for (let i = temp.length - 1; i >= 0; i--) {
   if (temp[i] !== ".") {
@@ -24,70 +24,6 @@ for (let i = temp.length - 1; i >= 0; i--) {
     break;
   }
 }
-console.log("last: ", last);
-// last would be the stopping point for our outer while loop
-
-// let update = true;
-// while (update) {
-//   console.log("temp: ", temp.toString());
-//   let start = 0;
-//   while (start < last) {
-//     if (temp[start] !== ".") {
-//       start++;
-//     } else {
-//       // first time we spot a space, at temp[start]
-
-//       // 1) check size of space
-//       let tempstart = start;
-//       let spaceSize = 0;
-//       while (temp[tempstart] === ".") {
-//         tempstart++;
-//         spaceSize++;
-//       }
-//       console.log("at index: ", start);
-//       console.log("we have space of size: ", spaceSize);
-
-//       // 2) from "last" index, find data that fits
-//       // move our last pointer up until we find a data that fits
-//       let found = false;
-//       while (last > start && found === false) {
-//         if (temp[last] === ".") {
-//           last--;
-//         } else {
-//           let curr = temp[last];
-//           let dataSize = 0;
-//           // count how big this data is
-//           while (temp[last] === curr) {
-//             dataSize++;
-//             last--;
-//           }
-//           last++;
-//           if (dataSize <= spaceSize) {
-//             // if data found is smaller than or equal to size of space
-//             console.log("can fit data: ", curr);
-//             console.log("datasize: ", dataSize);
-//             console.log("index of data: ", last);
-//             for (let l = 0; l < dataSize; l++) {
-//               [temp[start + l], temp[last + l]] = [
-//                 temp[last + l],
-//                 temp[start + l],
-//               ];
-//             }
-//             last -= dataSize;
-//             found = true;
-//             break;
-//           } else {
-//             // else we find next possible data
-//             last--;
-//           }
-//         }
-//       }
-
-//       // if found. swap.
-//       // else not found. move "last" index down till we find
-//     }
-//   }
-// }
 
 // our outer while loop will be from last until 0 or more
 // within the loop, we want to get the size of the file.
@@ -109,11 +45,56 @@ while (last >= 0) {
     }
     templast += 1;
     last = templast;
-    console.log("data, size: ", curr, dataSize);
-    console.log("at index start: ", templast);
+    // console.log("data, size: ", curr, dataSize);
+    // console.log("at index start: ", templast);
     // left pointer always start from 0, move left pointer up and find space and count.
     // left pointer should not exceed templast
     let left = 0;
-    while (left < )
+    let flag = false;
+    while (left < last) {
+      if (temp[left] !== ".") {
+        left++;
+      } else {
+        // find size of space.
+        let tempstart = left;
+        let spaceSize = 0;
+        while (temp[tempstart] === ".") {
+          tempstart++;
+          spaceSize++;
+        }
+        // console.log("space size: ", spaceSize);
+        // console.log("at index start: ", left);
+
+        // if spaceSize >= dataSize, we perform the swap
+        // else we move up left pointer and check
+        // move left pointer to after the end of this current space. set left = tempStart
+        if (spaceSize >= dataSize) {
+          for (let l = 0; l < dataSize; l++) {
+            [temp[left + l], temp[templast + l]] = [
+              temp[templast + l],
+              temp[left + l],
+            ];
+            // console.log("temp now: ", temp);
+            flag = true;
+          }
+          break;
+        } else {
+          left = tempstart;
+          continue;
+        }
+      }
+    }
+    if (flag === false) {
+      // console.log("cannot move this item: ", curr, dataSize);
+      last = templast - 1;
+    }
   }
 }
+// console.log("final tmep: ", temp);
+let output = 0;
+for (let i = 0; i < temp.length; i++) {
+  if (temp[i] !== ".") {
+    output += i * parseInt(temp[i]);
+  }
+}
+console.log("output: ", output);
