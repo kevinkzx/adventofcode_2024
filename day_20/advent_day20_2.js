1299e379e2d890a47f1c2f01dc02154e2884d39e
kevink <kevinkuerzx@gmail.com>
2024-12-22 23:41:54 +0800

    day 20 p2 idea

--- day_20/advent_day20_2.js
+++ day_20/advent_day20_2.js
@@ -1,2 +1,5 @@
 // maybe do BFS at each tile on the racetrack. and see that for any depths that is within 20 steps, whether this new tile is higher than where we started from.
 // add this start,end position into a set. and then count how many there are?
+
+// when doing BFS, anything up till 20 steps that is a ".", we check the tile number
+// also maybe in our BFS queue, we pass in the number of stesps it takes to get to this point, take the min?
