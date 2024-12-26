290489a2775eab86298b06f0a88d4e0e738364e6
kevink <kevinkuerzx@gmail.com>
2024-12-27 00:15:12 +0800

    day 24 p2 wip hardd

---
+++ day_24/advent_day24_2.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+let input = fs.readFileSync("advent_day24.txt").toString().split("\n");
+// console.log("input");
+// console.log(input);
+let datatemp = [];
+let instructiontemp = [];
+let tempflag = true;
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "") {
+    tempflag = false;
+    continue;
+  }
+  if (tempflag) {
+    datatemp.push(input[i]);
+  } else {
+    instructiontemp.push(input[i]);
+  }
+}
+// console.log("datatemp");
+// console.log(datatemp);
+console.log("instructiontemp");
+console.log(instructiontemp);
+
+let mydict = {};
+for (let i = 0; i < datatemp.length; i++) {
+  let address = datatemp[i].split(":")[0];
+  let value = datatemp[i].split(":")[1].trim();
+  // console.log("address: ", address);
+  // console.log("value: ", value);
+  mydict[address] = parseInt(value);
+}
+console.log("mydict: ", mydict);
+
+let xString = "";
+let yString = "";
+for (const [key, value] of Object.entries(mydict)) {
+  if (key[0] === "x") {
+    xString = mydict[key].toString() + xString;
+  } else {
+    yString = mydict[key].toString() + yString;
+  }
+}
+console.log("xString: ", xString);
+console.log("yString: ", yString);
